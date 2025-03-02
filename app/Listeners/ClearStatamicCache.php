d430ea68c84d73229eb7c503aad33dbba61ea2d7
brendan <16559757+Brennii96@users.noreply.github.com>
2025-03-02 11:41:31 +0000

    clear cache on post/collection save

---
+++ app/Listeners/ClearStatamicCache.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Cache;
+use Statamic\Events\EntrySaved;
+use Statamic\Facades\Stache;
+
+class ClearStatamicCache
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(EntrySaved $event): void
+    {
+        Stache::clear();
+        Cache::clear();
+    }
+}
