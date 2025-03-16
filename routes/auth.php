0be1bd5a471df8058261a1c351fadad99d9ee778
brendan <16559757+Brennii96@users.noreply.github.com>
2025-03-16 23:38:24 +0000

    add routes for subscriptions

--- routes/auth.php
+++ routes/auth.php
@@ -2,15 +2,18 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-
-//Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-//    ->middleware(['auth', 'signed', 'throttle:6,1'])
-//    ->name('verification.verify');
+use App\Http\Controllers\Members\SubscriptionController;
 
 Route::middleware('auth:member')->group(function () {
     Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
         ->middleware(['signed', 'throttle:6,1'])
         ->name('members.verification.verify');
+
+    Route::post('/members/{member}/subscribe', [SubscriptionController::class, 'subscribe']);
+    Route::post('/members/{member}/cancel', [SubscriptionController::class, 'cancel']);
+    Route::post('/members/{member}/resume', [SubscriptionController::class, 'resume']);
+    Route::get('/members/{member}/subscription-status', [SubscriptionController::class, 'subscriptionStatus']);
+    Route::post('/members/{member}/update-payment-method', [SubscriptionController::class, 'updatePaymentMethod']);
 });
 
 Route::get('/login', [AuthController::class, 'loginView'])
